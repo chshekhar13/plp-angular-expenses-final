2d569d23b458d78251c0150ec47a0894110b0aa9
chshekhar13 <36885940+chshekhar13@users.noreply.github.com>
2019-11-05 12:53:59 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+﻿
+import { ExpenseService } from './expense.service';
+
+import { Component } from '@angular/core';
+
+import { Expense } from 'src/model/Expense';
+
+import { NgIf } from '@angular/common';
+
+
+@Component({
+    selector: 'app-root',
+    templateUrl: 'app.component.html'
+})
+
+export class AppComponent {
+
+    updateNumber: number
+    expense_code: number
+    flag: number
+    expenses:  Expense[]
+    constructor(public ExpenseService: ExpenseService) {
+        ExpenseService.newExpense = new Expense()
+        ExpenseService.updatedExpense = new Expense()
+        
+        
+    }
+     getFunction() {
+         if(this.expense_code != null){
+            console.log(this.expense_code)
+            this.ExpenseService.getExpenseByCode(this.expense_code).subscribe(data => this.ExpenseService.Expenses = data)
+            this.flag=1
+        }
+        else{
+        console.log("Cannot be empty")
+        }
+    }
+    ngOnInit() {
+        this.ExpenseService.getAll().subscribe(res => {
+            this.expenses = res;
+          });
+          console.log(this.expenses)
+    }   
+  
+   }   
+
+
